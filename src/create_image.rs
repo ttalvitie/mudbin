ff8c1f509cc74ce86bbb5177a210b44853b56fde
Topi Talvitie <topi.talvitie@iki.fi>
2019-01-01 15:03:12 +0200

    Switched to TempDir from the tempfile crate and fixed formatting.

--- src/create_image.rs
+++ src/create_image.rs
@@ -8,8 +8,11 @@ use log::warn;
 
 pub fn create_image(_output_path: &Path) -> BoxFuture<()> {
     warn!("Image creation not implemented");
-    future::result(QemuConfig::new().and_then(|x| x.boot_kernel("../../preseedtest/linux", "../../preseedtest/initrd.gz")))
-        .and_then(|x| x.spawn())
-        .and_then(|x| x.wait())
-        .into_box()
+    future::result(
+        QemuConfig::new()
+            .and_then(|x| x.boot_kernel("../../preseedtest/linux", "../../preseedtest/initrd.gz")),
+    )
+    .and_then(|x| x.spawn())
+    .and_then(|x| x.wait())
+    .into_box()
 }
--- src/qemu.rs
+++ src/qemu.rs
@@ -5,13 +5,13 @@ use std::os::unix::fs::symlink;
 use std::path::Path;
 use std::process::Command;
 
-use tempdir::TempDir;
+use tempfile::{tempdir, TempDir};
 
 use tokio_process::{Child, CommandExt};
 
 pub struct Qemu {
     process: Child,
-    workdir: TempDir
+    workdir: TempDir,
 }
 
 impl Qemu {
@@ -20,7 +20,13 @@ impl Qemu {
         self.process
             .wait_success()
             .chain_err(|| "Error in child QEMU process")
-            .and_then(move |()| future::result(workdir.close().chain_err(|| "Closing temporary directory for QEMU failed")))
+            .and_then(move |()| {
+                future::result(
+                    workdir
+                        .close()
+                        .chain_err(|| "Closing temporary directory for QEMU failed"),
+                )
+            })
             .into_box()
     }
 }
@@ -28,12 +34,12 @@ impl Qemu {
 pub struct QemuConfig {
     command: Command,
     workdir: TempDir,
-    next_binding_idx: u64
+    next_binding_idx: u64,
 }
 
 impl QemuConfig {
     pub fn new() -> Result<QemuConfig> {
-        let workdir = TempDir::new("mudbin_tmp").chain_err(|| "Creating temporary directory for QEMU failed")?;
+        let workdir = tempdir().chain_err(|| "Creating temporary directory for QEMU failed")?;
 
         let mut command = Command::new("qemu-system-x86_64");
         command.current_dir(workdir.path());
@@ -43,7 +49,11 @@ impl QemuConfig {
         command.arg("-m").arg("1024");
         command.arg("-device").arg("virtio-serial");
 
-        Ok(QemuConfig{command, workdir, next_binding_idx: 0})
+        Ok(QemuConfig {
+            command,
+            workdir,
+            next_binding_idx: 0,
+        })
     }
 
     fn alloc_binding(&mut self) -> String {
@@ -57,13 +67,20 @@ impl QemuConfig {
         if !src.is_file() {
             return Err("QEMU binding source file is not a regular file".into());
         }
-        let src = src.canonicalize().chain_err(|| "Could not canonicalize QEMU binding source file path")?;
+        let src = src
+            .canonicalize()
+            .chain_err(|| "Could not canonicalize QEMU binding source file path")?;
         let binding = self.alloc_binding();
-        symlink(src, self.workdir.path().join(&binding)).chain_err(|| "Could not create symlink for QEMU file binding")?;
+        symlink(src, self.workdir.path().join(&binding))
+            .chain_err(|| "Could not create symlink for QEMU file binding")?;
         Ok(binding)
     }
 
-    pub fn boot_kernel<P: AsRef<Path>, Q: AsRef<Path>>(mut self, kernel: P, initrd: Q) -> Result<Self> {
+    pub fn boot_kernel<P: AsRef<Path>, Q: AsRef<Path>>(
+        mut self,
+        kernel: P,
+        initrd: Q,
+    ) -> Result<Self> {
         let kernel_binding = self.bind_file(kernel)?;
         let initrd_binding = self.bind_file(initrd)?;
         self.command.arg("-kernel").arg(kernel_binding);
@@ -72,9 +89,13 @@ impl QemuConfig {
     }
 
     pub fn spawn(mut self) -> Result<Qemu> {
-        let process = self.command
+        let process = self
+            .command
             .spawn_async()
             .chain_err(|| "Could not spawn child QEMU process")?;
-        Ok(Qemu { process, workdir: self.workdir })
+        Ok(Qemu {
+            process,
+            workdir: self.workdir,
+        })
     }
 }
